7026e1ef39757d3cec533bdf39832f6c1ecc127d
Mike Zrimsek <mikezrimsek@gmail.com>
2023-03-15 03:14:14 -0400

    add money hack

---
+++ hacks/moneyhack/README.md
@@ -0,0 +1,14 @@
+# Money Hack
+
+Finds the most recent file in your save backup directory, base64 decodes it into JSON, gives you a shitload of money, and then saves the updated save to a new JSON file.
+
+## Usage
+
+1. Export a save from the game into your backup directory
+2. Run `node moneyhack.js`
+3. Import the new JSON file into the game and confirm to overwrite the changes
+
+## Notes
+
+- You can always just reimport your backup if something goes wrong
+- Sometimes it seems like it takes two tries to get a save to import? I may just be dumb.
---
+++ hacks/moneyhack/moneyHack.js
@@ -0,0 +1,41 @@
+import { writeFileSync, readFileSync, readdirSync, lstatSync } from 'fs';
+import path from 'path';
+
+const orderRecentFiles = (dir) => {
+  return readdirSync(dir)
+    .filter((file) => lstatSync(path.join(dir, file)).isFile())
+    .map((file) => ({ file, mtime: lstatSync(path.join(dir, file)).mtime }))
+    .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
+};
+
+const getMostRecentFile = (dir) => {
+  const files = orderRecentFiles(dir);
+  return files.length ? files[0] : undefined;
+};
+
+const saveBackupDirectory = 'B:\\other\\games\\bitburner';
+
+const mostRecentSave = getMostRecentFile(saveBackupDirectory);
+
+if (mostRecentSave) {
+  const saveBase64 = readFileSync(path.join(saveBackupDirectory, mostRecentSave.file), 'utf8');
+  const encoded = atob(saveBase64);
+  const saveData = JSON.parse(encoded);
+  const playerSave = JSON.parse(saveData.data.PlayerSave);
+
+  console.log(playerSave);
+  
+  const currentMoney = playerSave.data.money;
+  const newMoney = currentMoney * 10000000000000;
+  playerSave.data.money = newMoney;
+
+  saveData.data.PlayerSave = JSON.stringify(playerSave);
+  const newSave = btoa(JSON.stringify(saveData));
+  writeFileSync('newsave.json', newSave, err => {
+    if (err) {
+      console.log('Error writing file', err);
+    } else {
+      console.log('Successfully wrote file');
+    }
+  });
+}
