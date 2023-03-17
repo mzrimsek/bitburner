341fc6c3d6aa5a2c63312c8a5d2ce19abe20b139
Mike Zrimsek <mikezrimsek@gmail.com>
2023-03-17 03:50:36 -0400

    add services for bot and hacknet
    goal is to use these to help consolidate behavior

---
+++ home/services/bots.js
@@ -0,0 +1,85 @@
+export default class BotService {
+  /** @param {import("..").NS } ns */
+  constructor(ns) {
+    this.ns = ns;
+  }
+
+  buyOrUpgradeBots() {
+    const bots = this.ns.getPurchasedServers().map(server => this.ns.getServer(server));
+    if (bots.length === 0) {
+      this._buyBot(bots);
+    } else {
+      const botToUpgrade = this._getBotToUpgrade(bots);
+      if (botToUpgrade) {
+        log(`upgrading ${botToUpgrade.hostname} for ${botToUpgrade.costToUpgrade}`);
+        this.ns.upgradePurchasedServer(botToUpgrade.hostname, botToUpgrade.targetRam);
+        lastServerAction = UPGRADE;
+        lastServerName = botToUpgrade.hostname;
+        lastServerPrice = botToUpgrade.costToUpgrade;
+        lastServerTime = new Date();
+      } else if (bots.length < MAX_BOTS) {
+        this._buyBot(bots);
+      }
+    }
+  }
+
+  /**
+   *  @param {import("..").Server[]} bots
+   */
+  _buyBot(bots) {
+    const nextIndex = this._getNextIndex(bots);
+    const nextBotName = `bot-${nextIndex}`;
+
+    const money = this.ns.getPlayer().money;
+    const cost = this.ns.getPurchasedServerCost(2);
+
+    if (money >= cost) {
+      log(`buying ${nextBotName}`);
+      this.ns.purchaseServer(nextBotName, 2);
+      lastServerAction = BUY;
+      lastServerName = nextBotName;
+      lastServerPrice = cost;
+      lastServerTime = new Date();
+    } else {
+      log('cannot buy bot at this time');
+    }
+  }
+
+  /**
+   *  @param {import("..").Server[]} bots
+   */
+  _getBotToUpgrade(bots) {
+    const mapped = bots.map(bot => {
+      return {
+        hostname: bot.hostname,
+        costToUpgrade: this.ns.getPurchasedServerUpgradeCost(bot.hostname, bot.maxRam * 2),
+        targetRam: bot.maxRam * 2
+      };
+    })
+      .filter(bot => {
+        const money = this.ns.getPlayer().money;
+        return money >= bot.costToUpgrade;
+      });
+
+    if (mapped.length === 0) {
+      return null;
+    }
+
+    return mapped.sort((bot1, bot2) => bot1.costToUpgrade - bot2.costToUpgrade)[0];
+  }
+
+  /**
+   *  @param {import("..").Server[]} bots
+   */
+  _getNextIndex(bots) {
+    if (bots.length === 0) {
+      return 1;
+    }
+    const botNames = bots.map(bot => bot.hostname);
+    const indices = botNames.map(botName => {
+      const parts = botName.split('-');
+      return parseInt(parts[1]);
+    });
+    return indices.sort((a, b) => b - a)[0] + 1;
+  }
+}
---
+++ home/services/hacknet.js
@@ -0,0 +1,127 @@
+export default class HacknetService {
+  /** @param {import("..").NS } ns */
+  constructor(ns) {
+    this.ns = ns;
+  }
+
+  purchaseUpgradeOrNode() {
+    const next = this._getNextUpgrade();
+    if (!next) {
+      const nodeCost = this.ns.hacknet.getPurchaseNodeCost();
+      const currentMoney = this.ns.getPlayer().money;
+
+      if (nodeCost <= currentMoney) {
+        log(`Buying new node for ${nodeCost}`);
+        const newIndex = this.ns.hacknet.purchaseNode();
+        const newStats = this.ns.hacknet.getNodeStats(newIndex);
+        lastAction = 'buy';
+        lastNode = newStats.name;
+        lastCost = nodeCost;
+        lastTime = new Date();
+      }
+    } else {
+      log(`(${next.name}) Upgrading ${next.upgrade}`);
+      const success = this._processUpgrade(next);
+      if (success) {
+        lastAction = next.upgrade;
+        lastNode = next.name;
+        lastCost = next.cost;
+        lastTime = new Date();
+        log(`(${next.name}) Successfully upgraded ${next.upgrade}`);
+      } else {
+        log(`(${next.name}) Failed to upgrade ${next.upgrade}`);
+      }
+    }
+  }
+
+  _getNextUpgrade() {
+    const nodeCount = this.ns.hacknet.numNodes();
+    const nodes = [...new Array(nodeCount)].map((_, index) => {
+      const nodeInfo = this.ns.hacknet.getNodeStats(index);
+      const level = {
+        current: nodeInfo.level,
+        upgradeCost: this.ns.hacknet.getLevelUpgradeCost(index, 1)
+      };
+      const ram = {
+        current: nodeInfo.ram,
+        upgradeCost: this.ns.hacknet.getRamUpgradeCost(index, 1)
+      };
+      const cores = {
+        current: nodeInfo.cores,
+        upgradeCost: this.ns.hacknet.getCoreUpgradeCost(index, 1)
+      };
+
+      return {
+        ...nodeInfo,
+        level: level,
+        ram: ram,
+        cores: cores,
+        index
+      };
+    });
+
+    const currentMoney = this.ns.getPlayer().money;
+    const potentialUpgrades = nodes.reduce((items, node) => {
+      if (node.level.current < maxLevel) {
+        if (node.level.upgradeCost <= currentMoney) {
+          items = [...items, {
+            upgrade: 'level',
+            index: node.index,
+            cost: node.level.upgradeCost,
+            name: node.name
+          }];
+        }
+      }
+
+      if (node.ram.current < maxRam) {
+        if (node.ram.upgradeCost <= currentMoney) {
+          items = [...items, {
+            upgrade: 'ram',
+            index: node.index,
+            cost: node.ram.upgradeCost,
+            name: node.name
+          }];
+        }
+      }
+
+      if (node.cores.current < maxCores) {
+        if (node.cores.upgradeCost <= currentMoney) {
+          items = [...items, {
+            upgrade: 'cores',
+            index: node.index,
+            cost: node.cores.upgradeCost,
+            name: node.name
+          }];
+        }
+      }
+
+      return items;
+    }, []);
+
+    if (potentialUpgrades.length === 0) {
+      return null;
+    }
+
+    log(`Found ${potentialUpgrades.length} potential upgrades`);
+    const sortedPotentialUpgrades = potentialUpgrades.sort((a, b) => a.cost - b.cost);
+
+    return sortedPotentialUpgrades[0];
+  }
+
+  _processUpgrade(next) {
+    switch (next.upgrade) {
+      case 'level': {
+        return this.ns.hacknet.upgradeLevel(next.index, 1);
+      }
+      case 'ram': {
+        return this.ns.hacknet.upgradeRam(next.index, 1);
+      }
+      case 'cores': {
+        return this.ns.hacknet.upgradeCore(next.index, 1);
+      }
+      default: {
+        return false;
+      }
+    }
+  }
+}
