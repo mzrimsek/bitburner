a9e49c7f33214ab8b10915237c0e6788862b8307
Mike Zrimsek <mikezrimsek@gmail.com>
2023-03-22 01:04:08 -0400

    wip sleeve automation

--- home/services/sleeves.js
+++ home/services/sleeves.js
@@ -13,18 +13,33 @@ export class SleeveService {
   handleSleeves(eventHandler) {
     const sleeves = this._getSleeves();
     sleeves.forEach((sleeve, index) => {
-      if (sleeve.shock > this.SHOCK_THRESHOLD) {
+      if (sleeve.shock > this.SHOCK_THRESHOLD && this.sleeve.getTask(index).type !== 'RECOVERY') {
         this.sleeve.setToShockRecovery(index);
         eventHandler && eventHandler({
           action: 'shock',
           name: `Sleeve ${index}`
         });
-      } else if (sleeve.sync < this.SYNC_THRESHOLD) {
+      } else if (sleeve.sync < this.SYNC_THRESHOLD && this.sleeve.getTask(index).type !== 'SYNCHRO') {
         this.sleeve.setToSynchronize(index);
         eventHandler && eventHandler({
           action: 'sync',
           name: `Sleeve ${index}`
         });
+      } else {
+        const augmentsForSale = this.sleeve.getSleevePurchasableAugs(index);
+        const affordableAugments = augmentsForSale.filter(augment => {
+          return this.ns.getPlayer().money >= augment.cost;
+        });
+        const augmentsByCost = affordableAugments.sort((a, b) => a.cost - b.cost);
+        if (augmentsByCost.length > 0) {
+          const augment = augmentsByCost[0];
+          this.sleeve.purchaseSleeveAug(index, augment.name);
+          eventHandler && eventHandler({
+            action: 'augment',
+            name: `Sleeve ${index}`,
+            augment: augment.name
+          });
+        }
       }
     });
   }
