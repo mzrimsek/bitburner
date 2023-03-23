dddf47dab674ae0143d698cf74bd0d13e38a6846
Mike Zrimsek <mikezrimsek@gmail.com>
2023-03-22 23:13:56 -0400

    not having to type --limit is easier

--- home/set/stonkLiquid.js
+++ home/set/stonkLiquid.js
@@ -2,18 +2,17 @@ import { PORT_MAPPING } from 'utils.js';
 
 /** @param {import("..").NS } ns */
 export async function main(ns) {
-  const newLimit = ns.args.find(arg => arg.startsWith('--limit='));
-  if (newLimit === undefined) {
-    ns.tprint('No limit provided');
+  if (ns.args.length !== 1) {
+    ns.tprint('Usage: stonkLiquid.js VALUE');
     ns.exit();
   }
 
-  const newLimitValue = parseInt(newLimit.split('=')[1], 10);
-  if (newLimitValue < 0 || isNaN(newLimitValue)) {
+  const newLimit = parseInt(ns.args[0], 10);
+  if (newLimit < 0 || isNaN(newLimit)) {
     ns.tprint('Limit must be 0 or greater');
     ns.exit();
   }
 
   ns.clearPort(PORT_MAPPING.STONKS_LIQUID_CASH_M);
-  ns.writePort(PORT_MAPPING.STONKS_LIQUID_CASH_M, newLimitValue);
+  ns.writePort(PORT_MAPPING.STONKS_LIQUID_CASH_M, newLimit);
 }
