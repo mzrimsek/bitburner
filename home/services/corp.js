da12e586d8987270f75aa919e04015cf90ba1e18
Mike Zrimsek <mikezrimsek@gmail.com>
2023-03-25 03:17:51 -0400

    stub out corp service

---
+++ home/services/corp.js
@@ -0,0 +1,13 @@
+export class CorpService {
+  /** @param {import("..").NS } ns */
+  constructor(ns) {
+    this.ns = ns;
+    this.corp = this.ns.corporation;
+  }
+
+  /** @param {import("..").ScriptHandler?} eventHandler */
+  handleCorporation(eventHandler) {
+
+  }
+
+}
