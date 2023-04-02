213691d6e0df7c0d787cbd92fed070ebff10c547
Mike Zrimsek <mikezrimsek@gmail.com>
2023-04-02 05:24:10 -0400

    add logic to start corp

--- home/services/startup.js
+++ home/services/startup.js
@@ -6,38 +6,51 @@ export class StartupService {
   constructor(ns, eventHandler = logEventHandler) {
     this.ns = ns;
     this.sing = ns.singularity;
+    this.corp = ns.corporation;
+    this.stock = ns.stock;
+
     this.eventHandler = eventHandler;
   }
 
   setupDarkweb() {
-    if (this.ns.getPlayer().money >= 200000) {
+    const darkWebCost = 200000; // 200k
+    if (this.#getCurrentMoney() >= darkWebCost) {
       this.sing.purchaseTor();
     }
 
     const darkWebProgramNames = this.sing.getDarkwebPrograms();
     darkWebProgramNames.forEach(programName => {
-      if (this.ns.getPlayer().money >= this.sing.getDarkwebProgramCost(programName)) {
+      if (this.#getCurrentMoney() >= this.sing.getDarkwebProgramCost(programName)) {
         this.sing.purchaseProgram(programName);
       }
     });
   }
 
   setupStonks() {
-    this.ns.stock.purchaseWseAccount();
-    this.ns.stock.purchaseTixApi();
-    this.ns.stock.purchase4SMarketData();
-    this.ns.stock.purchase4SMarketDataTixApi();
+    this.stock.purchaseWseAccount();
+    this.stock.purchaseTixApi();
+    this.stock.purchase4SMarketData();
+    this.stock.purchase4SMarketDataTixApi();
   }
 
   setupHome() {
     const ramCost = this.sing.getUpgradeHomeRamCost();
-    if (this.ns.getPlayer().money >= ramCost) {
+    if (this.#getCurrentMoney() >= ramCost) {
       this.sing.upgradeHomeRam();
     }
 
     const coreCost = this.sing.getUpgradeHomeCoresCost();
-    if (this.ns.getPlayer().money >= coreCost) {
+    if (this.#getCurrentMoney() >= coreCost) {
       this.sing.upgradeHomeCores();
     }
   }
+
+  setupCorp() {
+    const corpCost = 150000000000; // 150 billion
+    if (!this.corp.hasCorporation() && this.#getCurrentMoney() >= corpCost) {
+      this.corp.createCorporation('Nova Corp', true);
+    }
+  }
+
+  #getCurrentMoney = () => this.ns.getPlayer().money;
 }
