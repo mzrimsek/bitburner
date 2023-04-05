b1ad266238f922cf90c859b2418d9830d847b4df
Mike Zrimsek <mikezrimsek@gmail.com>
2023-04-04 23:41:03 -0400

    automate purchasing augments

--- home/services/faction.js
+++ home/services/faction.js
@@ -16,14 +16,54 @@ export class FactionService {
   }
 
   handleCurrentFactionAugments() {
+    const purchasedAndInstalledAugmentations = this.sing.getOwnedAugmentations(true);
+    const currentFactions = this.ns.getPlayer().factions;
     const currentJob = this.sing.getCurrentWork();
-    this.#log(`Current job: ${currentJob}`);
+
+    const currentFactionAugmentations = currentFactions.reduce((acc, faction) => {
+      const factionAugmentations = this.sing.getAugmentationsFromFaction(faction);
+      return [...acc, ...factionAugmentations];
+    }, []);
+
+    const currentFactionAugmentationsToBuy = currentFactionAugmentations.filter(
+      augmentName => !purchasedAndInstalledAugmentations.includes(augmentName)
+    );
+
+    const currentFactionAugmentationsToBuyWithDetails = currentFactionAugmentationsToBuy.map(
+      augmentName => {
+        const faction = currentFactions.find(faction =>
+          this.sing.getAugmentationsFromFaction(faction).includes(augmentName)
+        );
+        const cost = this.sing.getAugmentationPrice(augmentName);
+        const rep = this.sing.getAugmentationRepReq(augmentName);
+        const weight = currentJob.type === 'FACTION' && currentJob.factionName === faction ? 2 : 1;
+        return { augmentName, faction, cost, rep, weight };
+      }
+    );
+
+    const currentMoney = this.ns.getPlayer().money;
+    const afforableFactionAugmentationsToBuy = currentFactionAugmentationsToBuyWithDetails.filter(
+      ({ cost, faction, rep }) => {
+        const factionRep = this.sing.getFactionRep(faction);
+        return currentMoney >= cost && factionRep >= rep;
+      }
+    );
+
+    const sortedAfforableAugmentations = afforableFactionAugmentationsToBuy.sort(
+      (a, b) => a.cost - b.cost || b.weight - a.weight
+    );
+
+    const nextAfforableAugmentation = sortedAfforableAugmentations[0];
+    this.#log('nextAfforableAugmentation', nextAfforableAugmentation);
+    if (nextAfforableAugmentation) {
+      this.sing.purchaseAugmentation(
+        nextAfforableAugmentation.faction,
+        nextAfforableAugmentation.augmentName
+      );
+    }
   }
 
-  // if working for a faction
-  // and that faction has available augments that are affordable
-  // buy the cheapest augment until all available augments are bought
-  // maybe should also check any augments for current factions, but weight the ones for the current faction higher
+  // when getting an invite from CSEC, NiteSEC, etc, accept it automatically
 
   #log(...args) {
     utilLog('faction', ...args);
