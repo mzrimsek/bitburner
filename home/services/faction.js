4b1429459ca7e2dfeaf43aef26eae1f5069c29fc
Mike Zrimsek <mikezrimsek@gmail.com>
2023-04-05 02:21:50 -0400

    fix data to properly hvae a list of all factions that offer an augment so we can weight and select the proper entry

--- home/services/faction.js
+++ home/services/faction.js
@@ -28,41 +28,30 @@ export class FactionService {
     const currentFactions = this.ns.getPlayer().factions;
     const currentJob = this.sing.getCurrentWork();
 
-    const currentFactionAugmentations = currentFactions.reduce((acc, faction) => {
-      const factionAugmentations = this.sing.getAugmentationsFromFaction(faction);
+    const availableAugmentationInfo = currentFactions.reduce((acc, faction) => {
+      const factionAugmentations = this.sing
+        .getAugmentationsFromFaction(faction)
+        .filter(augmentName => !purchasedAndInstalledAugmentations.includes(augmentName))
+        .map(augmentName => {
+          const cost = this.sing.getAugmentationPrice(augmentName);
+          const rep = this.sing.getAugmentationRepReq(augmentName);
+          const weight = currentJob && currentJob.type === 'FACTION' ? 2 : 1;
+          return { augmentName, faction, cost, rep, weight };
+        });
       return [...acc, ...factionAugmentations];
     }, []);
 
-    const currentFactionAugmentationsToBuy = currentFactionAugmentations.filter(
-      augmentName => !purchasedAndInstalledAugmentations.includes(augmentName)
-    );
+    const elibleAugmentations = availableAugmentationInfo.filter(({ cost, faction, rep }) => {
+      const currentMoney = this.ns.getPlayer().money;
+      const factionRep = this.sing.getFactionRep(faction);
+      return currentMoney >= cost && factionRep >= rep;
+    });
 
-    const currentFactionAugmentationsToBuyWithDetails = currentFactionAugmentationsToBuy.map(
-      augmentName => {
-        const faction = currentFactions.find(faction =>
-          this.sing.getAugmentationsFromFaction(faction).includes(augmentName)
-        );
-        const cost = this.sing.getAugmentationPrice(augmentName);
-        const rep = this.sing.getAugmentationRepReq(augmentName);
-        const weight =
-          currentJob && currentJob.type === 'FACTION' && currentJob.factionName === faction ? 2 : 1;
-        return { augmentName, faction, cost, rep, weight };
-      }
-    );
-
-    const currentMoney = this.ns.getPlayer().money;
-    const afforableFactionAugmentationsToBuy = currentFactionAugmentationsToBuyWithDetails.filter(
-      ({ cost, faction, rep }) => {
-        const factionRep = this.sing.getFactionRep(faction);
-        return currentMoney >= cost && factionRep >= rep;
-      }
-    );
-
-    const sortedAfforableAugmentations = afforableFactionAugmentationsToBuy.sort(
+    const sortedElibleAugmentations = elibleAugmentations.sort(
       (a, b) => a.cost - b.cost || b.weight - a.weight
     );
 
-    const nextAfforableAugmentation = sortedAfforableAugmentations[0];
+    const nextAfforableAugmentation = sortedElibleAugmentations[0];
     if (nextAfforableAugmentation) {
       this.#log(
         `Purchasing ${nextAfforableAugmentation.augmentName} from ${nextAfforableAugmentation.faction}`
